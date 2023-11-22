536799a4c5868e05314aa91ff1a3c71c901dfdef
Vio <vio@beanon.com>
2023-11-22 22:36:42 +0100

    test: Add more assets/modules

--- test/unit/transform.test.ts
+++ test/unit/transform.test.ts
@@ -26,11 +26,52 @@ describe('bundleToWebpackStats', () => {
           isDynamicEntry: false,
           facadeModuleId: null,
           map: null,
-          moduleIds: [],
           isImplicitEntry: false,
           implicitlyLoadedBefore: [],
           importedBindings: {},
           referencedFiles: [],
+          moduleIds: [
+            path.join(ROOT_DIR, 'src/component-a.js'),
+            path.join(ROOT_DIR, 'src/index.js'),
+          ],
+          modules: {
+            [path.join(ROOT_DIR, 'src/component-a.js')]: {
+              code: 'export default A = 1;',
+              originalLength: 10,
+              renderedLength: 8,
+              removedExports: [],
+              renderedExports: [],
+            },
+            [path.join(ROOT_DIR, 'src/index.js')]: {
+              code: '',
+              originalLength: 100,
+              renderedLength: 80,
+              removedExports: [],
+              renderedExports: [],
+            },
+          },
+          imports: [],
+          exports: [],
+          dynamicImports: [],
+        },
+        'assets/vendors-abcd1234.js': {
+          name: 'vendors',
+          fileName: 'assets/vendors-abcd1234.js',
+          preliminaryFileName: 'assets/vendors-abcd1234.js',
+          type: 'chunk',
+          code: 'export default function () {}',
+          isEntry: true,
+          isDynamicEntry: false,
+          facadeModuleId: null,
+          map: null,
+          isImplicitEntry: false,
+          implicitlyLoadedBefore: [],
+          importedBindings: {},
+          referencedFiles: [],
+          moduleIds: [
+            path.join(ROOT_DIR, 'node_modules', 'package-a', 'index.js'),
+            path.join(ROOT_DIR, 'node_modules', 'package-b', 'index.js'),
+          ],
           modules: {
             [path.join(
               ROOT_DIR,
@@ -38,7 +79,7 @@ describe('bundleToWebpackStats', () => {
               'package-a',
               'index.js'
             )]: {
-              code: 'export default A = 1;',
+              code: '',
               originalLength: 10,
               renderedLength: 8,
               removedExports: [],
@@ -50,7 +91,41 @@ describe('bundleToWebpackStats', () => {
               'package-b',
               'index.js'
             )]: {
-              code: 'export default B = 2;',
+              code: '',
+              originalLength: 100,
+              renderedLength: 80,
+              removedExports: [],
+              renderedExports: [],
+            },
+          },
+          imports: [],
+          exports: [],
+          dynamicImports: [],
+        },
+        'assets/component-abcd1234.js': {
+          name: 'component',
+          fileName: 'assets/component-abcd1234.js',
+          preliminaryFileName: 'assets/component-abcd1234.js',
+          type: 'chunk',
+          code: 'export default function () {}',
+          isEntry: false,
+          isDynamicEntry: true,
+          facadeModuleId: null,
+          map: null,
+          isImplicitEntry: false,
+          implicitlyLoadedBefore: [],
+          importedBindings: {},
+          referencedFiles: [],
+          moduleIds: [
+            path.join(ROOT_DIR, 'src', 'component.js'),
+          ],
+          modules: {
+            [path.join(
+              ROOT_DIR,
+              'src',
+              'component.js',
+            )]: {
+              code: '',
               originalLength: 10,
               renderedLength: 8,
               removedExports: [],
@@ -72,6 +147,14 @@ describe('bundleToWebpackStats', () => {
           name: 'assets/main-abcd1234.js',
           size: 29,
         },
+        {
+          name: 'assets/vendors-abcd1234.js',
+          size: 29,
+        },
+        {
+          name: 'assets/component-abcd1234.js',
+          size: 29,
+        },
       ],
       chunks: [
         {
@@ -81,10 +164,40 @@ describe('bundleToWebpackStats', () => {
           names: ['main'],
           files: ['assets/main-abcd1234.js'],
         },
+        {
+          id: 'vendors',
+          initial: true,
+          entry: true,
+          names: ['vendors'],
+          files: ['assets/vendors-abcd1234.js'],
+        },
+        {
+          id: 'component',
+          initial: false,
+          entry: false,
+          names: ['component'],
+          files: ['assets/component-abcd1234.js'],
+        },
       ],
       modules: [
         {
           chunks: ['main'],
+          name: `.${path.sep}${path.join(
+            'src',
+            'component-a.js',
+          )}`,
+          size: 8,
+        },
+        {
+          chunks: ['main'],
+          name: `.${path.sep}${path.join(
+            'src',
+            'index.js'
+          )}`,
+          size: 80,
+        },
+        {
+          chunks: ['vendors'],
           name: `.${path.sep}${path.join(
             'node_modules',
             'package-a',
@@ -93,11 +206,19 @@ describe('bundleToWebpackStats', () => {
           size: 8,
         },
         {
-          chunks: ['main'],
+          chunks: ['vendors'],
           name: `.${path.sep}${path.join(
             'node_modules',
             'package-b',
             'index.js'
+          )}`,
+          size: 80,
+        },
+        {
+          chunks: ['component'],
+          name: `.${path.sep}${path.join(
+            'src',
+            'component.js',
           )}`,
           size: 8,
         },
