56891b099c4a82cb341aae2148e01e9e9547e79f
Viorel Cojocaru <vio@beanon.com>
2024-12-18 22:57:59 +0100

    fix: Set chunk initial flag based on the looked up value of the issuing chunks tree

---
+++ test/unit/lookup-chunk-async.test.ts
@@ -0,0 +1,196 @@
+import { describe, test, expect } from 'vitest';
+import type { OutputChunk } from 'rollup';
+
+import { lookupChunkAsync } from '../../src/transform';
+
+const COMMON_DATA: OutputChunk = {
+  name: '',
+  fileName: '',
+  preliminaryFileName: '',
+  sourcemapFileName: '',
+  type: 'chunk',
+  code: 'export default function () {}',
+  isEntry: false,
+  isDynamicEntry: false,
+  facadeModuleId: null,
+  map: null,
+  isImplicitEntry: false,
+  implicitlyLoadedBefore: [],
+  importedBindings: {},
+  referencedFiles: [],
+  moduleIds: [],
+  modules: {},
+  imports: [],
+  exports: [],
+  dynamicImports: [],
+};
+
+describe('lookupChunkAsync', () => {
+  describe('isDynamicEntry === true', () => {
+    test('chunk is async', () => {
+      /**
+       * A ---async---> PARENT?
+       */
+      expect(
+        lookupChunkAsync(
+          { ...COMMON_DATA, fileName: 'A', isDynamicEntry: true },
+          {}
+        )
+      ).toEqual(true);
+    });
+  });
+
+  describe('isDynamicEntry === false', () => {
+    test("chunk is sync if it is an entry", () => {
+      /**
+       * A ---sync---> ENTRY
+       */
+      expect(
+        lookupChunkAsync(
+          { ...COMMON_DATA, fileName: 'A', isDynamicEntry: false, isEntry: true },
+          {
+            A: ['B', 'C'],
+          }
+        )
+      ).toEqual(false);
+    });
+
+    test("chunk is sync if it doesn't have any issuers", () => {
+      /**
+       * A ---sync---> NO_PARENT
+       */
+      expect(
+        lookupChunkAsync(
+          { ...COMMON_DATA, fileName: 'A', isDynamicEntry: false },
+          {}
+        )
+      ).toEqual(false);
+    });
+
+    test('chunk is async when all chunk issuers are async', () => {
+      /**
+       * C <---sync--- A ---async---> ROOT
+       * C <---sync--- B ---async---/
+       */
+      expect(
+        lookupChunkAsync(
+          { ...COMMON_DATA, fileName: 'C', isDynamicEntry: false },
+          {
+            C: [
+              {
+                ...COMMON_DATA,
+                fileName: 'A',
+                isDynamicEntry: true,
+              },
+              {
+                ...COMMON_DATA,
+                fileName: 'B',
+                isDynamicEntry: true,
+              },
+            ],
+          }
+        )
+      ).toEqual(true);
+      /**
+       * E <--sync--- C <---sync--- A ---async---> ROOT
+       * E <--sync--- D <---sync--- B ---async---/
+       */
+      expect(
+        lookupChunkAsync(
+          { ...COMMON_DATA, fileName: 'E', isDynamicEntry: false },
+          {
+            E: [
+              {
+                ...COMMON_DATA,
+                fileName: 'C',
+                isDynamicEntry: false,
+              },
+              {
+                ...COMMON_DATA,
+                fileName: 'D',
+                isDynamicEntry: false,
+              },
+            ],
+            D: [
+              {
+                ...COMMON_DATA,
+                fileName: 'B',
+                isDynamicEntry: true,
+              },
+            ],
+            C: [
+              {
+                ...COMMON_DATA,
+                fileName: 'A',
+                isDynamicEntry: true,
+              },
+            ],
+          }
+        )
+      ).toEqual(true);
+    });
+
+    test('chunk is sync when at least one issuer is sync', () => {
+      /**
+       * C <--sync--- A ---async---> ROOT
+       * C <--sync--- B --- sync---/
+       */
+      expect(
+        lookupChunkAsync(
+          { ...COMMON_DATA, fileName: 'C', isDynamicEntry: false },
+          {
+            C: [
+              {
+                ...COMMON_DATA,
+                fileName: 'A',
+                isDynamicEntry: true,
+              },
+              {
+                ...COMMON_DATA,
+                fileName: 'B',
+                isDynamicEntry: false,
+              },
+            ],
+          }
+        )
+      ).toEqual(false);
+      /**
+       * E <---sync--- C <---sync--- A ---async---> ROOT
+       * E <---sync--- D <---sync--- B --- sync---/
+       */
+      expect(
+        lookupChunkAsync(
+          { ...COMMON_DATA, fileName: 'E', isDynamicEntry: false },
+          {
+            E: [
+              {
+                ...COMMON_DATA,
+                fileName: 'C',
+                isDynamicEntry: false,
+              },
+              {
+                ...COMMON_DATA,
+                fileName: 'D',
+                isDynamicEntry: false,
+              },
+            ],
+            D: [
+              {
+                ...COMMON_DATA,
+                fileName: 'B',
+                isDynamicEntry: false,
+              },
+            ],
+            C: [
+              {
+                ...COMMON_DATA,
+                fileName: 'A',
+                isDynamicEntry: true,
+              },
+            ],
+          }
+        )
+      ).toEqual(false);
+    });
+  });
+});
--- test/unit/transform.test.ts
+++ test/unit/transform.test.ts
@@ -13,11 +13,11 @@ describe('bundleToWebpackStats', () => {
           size: 11,
         },
         {
-          name: 'assets/main-abcd1234.js',
+          name: 'assets/vendors-abcd1234.js',
           size: 29,
         },
         {
-          name: 'assets/vendors-abcd1234.js',
+          name: 'assets/main-abcd1234.js',
           size: 29,
         },
         {
@@ -31,18 +31,18 @@ describe('bundleToWebpackStats', () => {
       ],
       chunks: [
         {
-          id: 'e1c35b4',
-          initial: true,
-          entry: true,
-          names: ['main'],
-          files: ['assets/main-abcd1234.js'],
-        },
-        {
           id: '95848fd',
           initial: true,
           entry: true,
           names: ['vendors'],
           files: ['assets/vendors-abcd1234.js'],
+        },
+        {
+          id: 'e1c35b4',
+          initial: true,
+          entry: true,
+          names: ['main'],
+          files: ['assets/main-abcd1234.js'],
         },
         {
           id: 'e7b195f',
@@ -61,16 +61,6 @@ describe('bundleToWebpackStats', () => {
       ],
       modules: [
         {
-          chunks: ['e1c35b4'],
-          name: `.${path.sep}${path.join('src', 'component-a.js')}`,
-          size: 8,
-        },
-        {
-          chunks: ['e1c35b4'],
-          name: `.${path.sep}${path.join('src', 'index.js')}`,
-          size: 80,
-        },
-        {
           chunks: ['95848fd'],
           name: `.${path.sep}${path.join(
             'node_modules',
@@ -86,6 +76,16 @@ describe('bundleToWebpackStats', () => {
             'package-b',
             'index.js'
           )}`,
+          size: 80,
+        },
+        {
+          chunks: ['e1c35b4'],
+          name: `.${path.sep}${path.join('src', 'component-a.js')}`,
+          size: 8,
+        },
+        {
+          chunks: ['e1c35b4'],
+          name: `.${path.sep}${path.join('src', 'index.js')}`,
           size: 80,
         },
         {
@@ -204,11 +204,11 @@ describe('bundleToWebpackStats', () => {
           size: 11,
         },
         {
-          name: 'assets/main-abcd1234.js',
+          name: 'assets/vendors-abcd1234.js',
           size: 29,
         },
         {
-          name: 'assets/vendors-abcd1234.js',
+          name: 'assets/main-abcd1234.js',
           size: 29,
         },
         {
@@ -222,18 +222,18 @@ describe('bundleToWebpackStats', () => {
       ],
       chunks: [
         {
-          id: 'e1c35b4',
-          initial: true,
-          entry: true,
-          names: ['main'],
-          files: ['assets/main-abcd1234.js'],
-        },
-        {
           id: '95848fd',
           initial: true,
           entry: true,
           names: ['vendors'],
           files: ['assets/vendors-abcd1234.js'],
+        },
+        {
+          id: 'e1c35b4',
+          initial: true,
+          entry: true,
+          names: ['main'],
+          files: ['assets/main-abcd1234.js'],
         },
         {
           id: 'e7b195f',
@@ -252,6 +252,15 @@ describe('bundleToWebpackStats', () => {
       ],
       modules: [
         {
+          chunks: ['95848fd'],
+          name: `.${path.sep}${path.join(
+            'node_modules',
+            'package-a',
+            'index.js'
+          )}`,
+          size: 8,
+        },
+        {
           chunks: ['e1c35b4'],
           name: `.${path.sep}${path.join('src', 'component-a.js')}`,
           size: 8,
@@ -260,15 +269,6 @@ describe('bundleToWebpackStats', () => {
           chunks: ['e1c35b4'],
           name: `.${path.sep}${path.join('src', 'index.js')}`,
           size: 80,
-        },
-        {
-          chunks: ['95848fd'],
-          name: `.${path.sep}${path.join(
-            'node_modules',
-            'package-a',
-            'index.js'
-          )}`,
-          size: 8,
         },
         {
           chunks: ['e7b195f'],
@@ -362,12 +362,12 @@ describe('bundleToWebpackStats', () => {
           type: 'asset',
         },
         {
-          name: 'assets/main-abcd1234.js',
+          name: 'assets/vendors-abcd1234.js',
           size: 29,
           type: 'chunk',
         },
         {
-          name: 'assets/vendors-abcd1234.js',
+          name: 'assets/main-abcd1234.js',
           size: 29,
           type: 'chunk',
         },
@@ -384,19 +384,19 @@ describe('bundleToWebpackStats', () => {
       ],
       chunks: [
         {
-          id: 'e1c35b4',
-          initial: true,
-          entry: true,
-          names: ['main'],
-          files: ['assets/main-abcd1234.js'],
-          type: 'chunk',
-        },
-        {
           id: '95848fd',
           initial: true,
           entry: true,
           names: ['vendors'],
           files: ['assets/vendors-abcd1234.js'],
+          type: 'chunk',
+        },
+        {
+          id: 'e1c35b4',
+          initial: true,
+          entry: true,
+          names: ['main'],
+          files: ['assets/main-abcd1234.js'],
           type: 'chunk',
         },
         {
@@ -418,18 +418,6 @@ describe('bundleToWebpackStats', () => {
       ],
       modules: [
         {
-          chunks: ['e1c35b4'],
-          name: `.${path.sep}${path.join('src', 'component-a.js')}`,
-          size: 8,
-          removedExportsLength: 0,
-        },
-        {
-          chunks: ['e1c35b4'],
-          name: `.${path.sep}${path.join('src', 'index.js')}`,
-          size: 80,
-          removedExportsLength: 0,
-        },
-        {
           chunks: ['95848fd'],
           name: `.${path.sep}${path.join(
             'node_modules',
@@ -446,6 +434,18 @@ describe('bundleToWebpackStats', () => {
             'package-b',
             'index.js'
           )}`,
+          size: 80,
+          removedExportsLength: 0,
+        },
+        {
+          chunks: ['e1c35b4'],
+          name: `.${path.sep}${path.join('src', 'component-a.js')}`,
+          size: 8,
+          removedExportsLength: 0,
+        },
+        {
+          chunks: ['e1c35b4'],
+          name: `.${path.sep}${path.join('src', 'index.js')}`,
           size: 80,
           removedExportsLength: 0,
         },
@@ -493,11 +493,11 @@ describe('bundleToWebpackStats', () => {
           size: 11,
         },
         {
-          name: 'assets/main-abcd1234.js',
+          name: 'assets/vendors-abcd1234.js',
           size: 29,
         },
         {
-          name: 'assets/vendors-abcd1234.js',
+          name: 'assets/main-abcd1234.js',
           size: 29,
         },
         {
@@ -511,18 +511,18 @@ describe('bundleToWebpackStats', () => {
       ],
       chunks: [
         {
-          id: 'e1c35b4',
-          initial: true,
-          entry: true,
-          names: ['main'],
-          files: ['assets/main-abcd1234.js'],
-        },
-        {
           id: '95848fd',
           initial: true,
           entry: true,
           names: ['vendors'],
           files: ['assets/vendors-abcd1234.js'],
+        },
+        {
+          id: 'e1c35b4',
+          initial: true,
+          entry: true,
+          names: ['main'],
+          files: ['assets/main-abcd1234.js'],
         },
         {
           id: 'e7b195f',
@@ -541,6 +541,36 @@ describe('bundleToWebpackStats', () => {
       ],
       modules: [
         {
+          chunks: ['95848fd'],
+          name: `.${path.sep}${path.join(
+            'node_modules',
+            'package-a',
+            'index.js'
+          )}`,
+          identifier: path.join(
+            ROOT_DIR,
+            'node_modules',
+            'package-a',
+            'index.js'
+          ),
+          size: 8,
+        },
+        {
+          chunks: ['95848fd'],
+          name: `.${path.sep}${path.join(
+            'node_modules',
+            'package-b',
+            'index.js'
+          )}`,
+          identifier: path.join(
+            ROOT_DIR,
+            'node_modules',
+            'package-b',
+            'index.js'
+          ),
+          size: 80,
+        },
+        {
           chunks: ['e1c35b4'],
           name: `.${path.sep}${path.join('src', 'component-a.js')}`,
           identifier: path.join(ROOT_DIR, 'src', 'component-a.js'),
@@ -550,36 +580,6 @@ describe('bundleToWebpackStats', () => {
           chunks: ['e1c35b4'],
           name: `.${path.sep}${path.join('src', 'index.js')}`,
           identifier: path.join(ROOT_DIR, 'src', 'index.js'),
-          size: 80,
-        },
-        {
-          chunks: ['95848fd'],
-          name: `.${path.sep}${path.join(
-            'node_modules',
-            'package-a',
-            'index.js'
-          )}`,
-          identifier: path.join(
-            ROOT_DIR,
-            'node_modules',
-            'package-a',
-            'index.js'
-          ),
-          size: 8,
-        },
-        {
-          chunks: ['95848fd'],
-          name: `.${path.sep}${path.join(
-            'node_modules',
-            'package-b',
-            'index.js'
-          )}`,
-          identifier: path.join(
-            ROOT_DIR,
-            'node_modules',
-            'package-b',
-            'index.js'
-          ),
           size: 80,
         },
         {
@@ -638,11 +638,11 @@ describe('bundleToWebpackStats', () => {
           size: 11,
         },
         {
-          name: 'assets/main-abcd1234.js',
+          name: 'assets/vendors-abcd1234.js',
           size: 29,
         },
         {
-          name: 'assets/vendors-abcd1234.js',
+          name: 'assets/main-abcd1234.js',
           size: 29,
         },
         {
@@ -656,18 +656,18 @@ describe('bundleToWebpackStats', () => {
       ],
       chunks: [
         {
-          id: 'e1c35b4',
-          initial: true,
-          entry: true,
-          names: ['main'],
-          files: ['assets/main-abcd1234.js'],
-        },
-        {
           id: '95848fd',
           initial: true,
           entry: true,
           names: ['vendors'],
           files: ['assets/vendors-abcd1234.js'],
+        },
+        {
+          id: 'e1c35b4',
+          initial: true,
+          entry: true,
+          names: ['main'],
+          files: ['assets/main-abcd1234.js'],
         },
         {
           id: 'e7b195f',
@@ -686,16 +686,6 @@ describe('bundleToWebpackStats', () => {
       ],
       modules: [
         {
-          chunks: ['e1c35b4'],
-          name: `.${path.sep}${path.join('src', 'component-a.js')}`,
-          size: 8,
-        },
-        {
-          chunks: ['e1c35b4'],
-          name: `.${path.sep}${path.join('src', 'index.js')}`,
-          size: 80,
-        },
-        {
           chunks: ['95848fd'],
           name: `.${path.sep}${path.join(
             'node_modules',
@@ -711,6 +701,16 @@ describe('bundleToWebpackStats', () => {
             'package-b',
             'index.js'
           )}`,
+          size: 80,
+        },
+        {
+          chunks: ['e1c35b4'],
+          name: `.${path.sep}${path.join('src', 'component-a.js')}`,
+          size: 8,
+        },
+        {
+          chunks: ['e1c35b4'],
+          name: `.${path.sep}${path.join('src', 'index.js')}`,
           size: 80,
         },
         {
