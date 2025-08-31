ce3836b3aa5a068c9b2c88adf6e374a81212cdda
Viorel Cojocaru <vio@beanon.com>
2025-08-31 15:29:15 +0200

    test: Add vite package test

---
+++ test/package/vite/options.test.ts
@@ -0,0 +1,45 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { beforeEach, describe, test, expect } from 'vitest';
+import { build as vite } from 'vite';
+import { vol } from 'memfs';
+
+import viteConfigs from './case-options.mjs';
+
+describe('vite options', () => {
+  beforeEach(() => {
+    vol.reset();
+  });
+
+  test('should output bundle stats JSON file when options is an object', async () => {
+    const config = viteConfigs[0];
+    await vite(config);
+
+    const actual = await fs.readFile(path.join(config.output.dir, 'webpack-stats.json'), 'utf8');
+    const stats = JSON.parse(actual);
+    expect(stats).toMatchObject({
+      assets: [
+        {
+          name: 'assets/index.js',
+          size: 739,
+        },
+      ],
+    })
+  });
+
+  test('should output bundle stats JSON file when options is a builder function', async () => {
+    const config = viteConfigs[1];
+    await vite(config);
+
+    const actual = await fs.readFile(path.join(config.build.outDir, 'stats-dist2.json'), 'utf8');
+    const stats = JSON.parse(actual);
+    expect(stats).toMatchObject({
+      assets: [
+        {
+          name: 'assets/index.js',
+          size: 739,
+        },
+      ],
+    })
+  });
+});
---
+++ test/package/vite/src/index.js
@@ -0,0 +1,4 @@
+(() => {
+  const message = 'hello world!';
+  console.log(message);
+})();
