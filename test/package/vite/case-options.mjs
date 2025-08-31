f68332d54ce83d4af886685492cdbdc6817eb5fe
Viorel Cojocaru <vio@beanon.com>
2025-08-31 15:29:15 +0200

    test: Add vite package test

---
+++ test/package/vite/case-options.mjs
@@ -0,0 +1,37 @@
+import { basename } from 'path';
+import { defineConfig } from 'vite';
+import webpackStats from 'rollup-plugin-webpack-stats';
+
+const baseConfig = {
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
+      },
+    },
+  },
+};
+
+export default defineConfig([
+  {
+    ...baseConfig,
+    plugins: [webpackStats()],
+  },
+  {
+    ...baseConfig,
+    build: {
+      ...baseConfig.build,
+    },
+    plugins: [
+      // A contrived demo to show that plugin options can access vite outputOptions
+      webpackStats((outputOptions) => {
+        return {
+          fileName: `stats-${basename(outputOptions.dir)}.json`,
+        };
+      }),
+    ],
+  },
+]);
