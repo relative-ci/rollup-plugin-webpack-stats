4efc6c73c0a0d63c2e90df40a5bac68921a2a1f0
Viorel Cojocaru <vio@beanon.com>
2025-09-28 12:53:46 +0200

    build: Verdaccio - tag and no auth

--- scripts/setup-registry.sh
+++ scripts/setup-registry.sh
@@ -6,6 +6,5 @@ npx verdaccio --config ./verdaccio.yml &
 sleep 10
 
 npm config set registry=http://localhost:4873
-npm config set //localhost:4873/:_authToken=fooBar # Add a non empty token for npm 6
 
-npm publish
+npm publish --tag
