467180e013522018c7886717a1b88f185a77bb48
Viorel Cojocaru <vio@beanon.com>
2025-09-28 14:04:58 +0200

    FIXUP: verdaccio

--- scripts/setup-registry.sh
+++ scripts/setup-registry.sh
@@ -6,5 +6,6 @@ npx verdaccio --config ./verdaccio.yml &
 sleep 10
 
 npm config set registry=http://localhost:4873
+npm config set //localhost:4873/:_authToken=fooBar # Add a non empty token for npm 6
 
 npm publish --tag beta
