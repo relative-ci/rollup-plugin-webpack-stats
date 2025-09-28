34e0feddf0e144238150d2b1fc7315a8ad39db2a
Viorel Cojocaru <vio@beanon.com>
2025-09-28 12:55:26 +0200

    refactor: release-it configs - convert to esm

--- config/release-it/common.js
+++ config/release-it/common.js
@@ -1,14 +1,10 @@
-require('dotenv').config();
-
-module.exports = {
+export default {
   git: {
     commit: true,
-    // eslint-disable-next-line no-template-curly-in-string
     commitMessage: ':package: release ${version}\n[ci skip]',
     push: true,
     requireUpstream: false,
     tag: true,
-    // eslint-disable-next-line no-template-curly-in-string
     tagName: 'v${version}'
   },
   npm: {
