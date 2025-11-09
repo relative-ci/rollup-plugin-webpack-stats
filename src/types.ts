031ca4356fcbec1084c6ceed8a0dc8b8a2469e58
Vio <vio@beanon.com>
2025-11-09 17:23:59 +0100

    FIXUP: use vite plugin type

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 import type { Plugin as RollupPlugin, OutputOptions as RollupOutputOptions } from 'rollup';
 import type { Plugin as VitePlugin } from 'vite';
 
-export type Plugin = RollupPlugin | VitePlugin;
+export type Plugin = VitePlugin;
 export type OutputOptions = RollupOutputOptions;
