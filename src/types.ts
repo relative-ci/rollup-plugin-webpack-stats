dee4c23dd76ac697ac865570cdc96d59ac04ab65
Vio <vio@beanon.com>
2025-11-09 17:24:39 +0100

    FIXUP

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import type { Plugin as RollupPlugin, OutputOptions as RollupOutputOptions } from 'rollup';
+import type { OutputOptions as RollupOutputOptions } from 'rollup';
 import type { Plugin as VitePlugin } from 'vite';
 
 export type Plugin = VitePlugin;
