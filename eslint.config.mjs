8361bc2249f50780638d798075f60480a42fd501
Viorel Cojocaru <vio@beanon.com>
2026-02-15 12:12:34 +0100

    build: Setup prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import configPrettier from 'eslint-config-prettier/flat';
 
 /** @type {import('eslint').Linter.Config[]} */
 const configs = [
@@ -13,10 +14,11 @@ const configs = [
     },
   },
   {
-    ignores: ['**/dist/'],
+    ignores: ['config/', '**/dist/'],
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  configPrettier,
 ];
 
 export default configs;
